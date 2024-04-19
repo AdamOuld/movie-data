9046db35d1972671db5eff0936965202148c036a
AdamOuld <ouldsfiyaadam@gmail.com>
2024-04-19 19:41:21 -0400

    added json file sorted by name

--- sortbyname.py
+++ sortbyname.py
@@ -4,12 +4,17 @@ from os import path
 
 import time
 
-filename = "data.json"
+from PyMovieDb import IMDB
 
-if path.isfile(filename) is False:
+imdb = IMDB()
+
+
+data_filename = "data.json"
+
+if path.isfile(data_filename) is False:
     raise Exception("File not found")
 
-with open(filename) as f:
+with open(data_filename) as f:
     movieList = json.load(f)
 
 movieNameList = [obj['name'] for obj in movieList if 'name' in obj]
@@ -34,38 +39,43 @@ def insertion_sort(arr):
 
 
 
-def merge_sort(arr):
-    if len(arr) > 1:
-        mid = len(arr) // 2
-        left_half = arr[:mid]
-        right_half = arr[mid:]
-
-        merge_sort(left_half)
-        merge_sort(right_half)
-
-        i = j = k = 0
-
-        while i < len(left_half) and j < len(right_half):
-            if left_half[i] < right_half[j]:
-                arr[k] = left_half[i]
-                i += 1
-            else:
-                arr[k] = right_half[j]
-                j += 1
-            k += 1
-
-        while i < len(left_half):
-            arr[k] = left_half[i]
-            i += 1
-            k += 1
-
-        while j < len(right_half):
-            arr[k] = right_half[j]
-            j += 1
-            k += 1
 
 
-
-track_time(merge_sort, movieNameList)
+track_time(insertion_sort, movieNameList)
 
 print(movieNameList)
+
+
+'''
+filename = "sortedbyname.json"
+
+jsonObj = []
+
+for movie in movieNameList:
+    res = imdb.get_by_name(movie, tv=False)
+    data = json.loads(res)
+    if not (data.get('review') is None):
+        del data['review']
+    if not (data.get('description') is None):
+        del data['description']
+    if not (data.get('actor') is None):
+        del data['actor']
+    if not (data.get('creator') is None):
+        del data['creator']
+    if not (data.get('keywords') is None):
+        del data['keywords']
+    if not (data.get('director') is None):
+        del data['director']
+    jsonObj.append(data)
+
+
+with open(filename, 'w') as json_file:
+    json.dump(jsonObj, json_file,
+              indent=4,
+              separators=(',', ': '))
+
+print('Successfully appended to the JSON file')
+
+'''
+
+
