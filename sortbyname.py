44e1097189161761469157d034e0cc490ad5fa07
AdamOuld <ouldsfiyaadam@gmail.com>
2024-04-19 19:13:50 -0400

    quick fix

--- sortbyname.py
+++ sortbyname.py
@@ -4,8 +4,6 @@ from os import path
 
 import time
 
-start_time = time.time()
-
 filename = "data.json"
 
 if path.isfile(filename) is False:
@@ -14,7 +12,15 @@ if path.isfile(filename) is False:
 with open(filename) as f:
     movieList = json.load(f)
 
-movieNameDict = [obj['name'] for obj in movieList if 'name' in obj]
+movieNameList = [obj['name'] for obj in movieList if 'name' in obj]
+
+print(movieNameList)
+
+def track_time(sort, arr):
+    start_time = time.perf_counter()
+    sort(arr)
+    end_time = time.perf_counter()
+    print("The sorting algorithm took", abs(start_time - end_time), "seconds to run")
 
 
 def insertion_sort(arr):
@@ -27,8 +33,39 @@ def insertion_sort(arr):
         arr[j + 1] = key
 
 
-insertion_sort(movieNameDict)
 
-print(movieNameDict)
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
 
-print("This algorithm took", time.time() - start_time, "seconds to run")
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = j = k = 0
+
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+
+
+track_time(merge_sort, movieNameList)
+
+print(movieNameList)
